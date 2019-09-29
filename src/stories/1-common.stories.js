b6eff15a31688ea700fe91c762b9d40c475bffad
Ahmed <sultan_902002@yahoo.com>
2019-09-29 16:47:35 -0600

    move more common components

---
+++ src/stories/1-common.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  Button  from 'components/common/Button';
+
+export default {
+  title: 'common',
+};
+
+export const confirm_button = () =>
+<Button confirm>Confirm</Button>;
+export const cancel_button = () =>
+<Button cancel>Cancel</Button>;
