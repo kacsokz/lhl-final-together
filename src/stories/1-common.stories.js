d0403415829d7386b42da43fcb21905f0ffafb3f
Ahmed <sultan_902002@yahoo.com>
2019-10-01 11:09:33 -0600

    refactor code and add userView to view the components based on the state

--- src/stories/1-common.stories.js
+++ src/stories/1-common.stories.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import  Button  from 'components/common/Button';
+import  Confirm  from 'components/common/Confirm';
+import  Error  from 'components/common/Error';
+import  Status  from 'components/common/Status';
 
 export default {
   title: 'common',
@@ -7,5 +10,15 @@ export default {
 
 export const confirm_button = () =>
 <Button confirm>Confirm</Button>;
+
 export const cancel_button = () =>
 <Button cancel>Cancel</Button>;
+
+export const Confirm_view = () =>
+<Confirm></Confirm>;
+
+export const Error_view = () =>
+<Error></Error>;
+
+export const Status_view = () =>
+<Status></Status>;
--- src/stories/3-User.stories.js
+++ src/stories/3-User.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Profile from 'components/User/profile';
 import EventsList from 'components/User/eventsList';
+import UserView from 'components/User/index';
 
 export default {
   title: 'User',
@@ -14,11 +15,21 @@ let events = [
   { id: "3", name: "EventName3", start_time: "15:00", end_time: "16:40", happyHourName: 'something5' }
 ]
 
+let user = [
+  { id: "1", first_name: "firstName1", last_name: "lastName2", email: "email@email.email", avatar: 'https://www.straitstimes.com/sites/default/files/styles/article_pictrure_780x520_/public/articles/2018/02/02/2017-09-05t123641z_2074388227_rc1f50c4add0_rtrmadp_3_filmfestival-venice.jpg?itok=oNSajv26&timestamp=1517521109', tag_line: 'this is tagline' }
+];
+
 export const profile = () =>
-  <Profile></Profile>;
+  <Profile user={user}></Profile>;
 
 export const event = () =>
   <EventsList
     events={events}
-  >
+    user={user}>
   </EventsList>;
+
+export const user_view = () =>
+  <UserView
+    user={user}
+    events={events}>
+  </UserView>;
