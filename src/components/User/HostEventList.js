929f84afe94ba9d4abb44d2b0a04941a28072556
Ahmed <sultan_902002@yahoo.com>
2019-10-06 13:39:45 -0600

    add on click listener to view the hosted event

--- src/components/User/HostEventList.js
+++ src/components/User/HostEventList.js
@@ -7,11 +7,13 @@ export default function HostEventList(props) {
   const userEvents = props.events.map(event => (
     <HostEventListItem
       key={event.id}
+      id={event.id}
       name={event.bar_name}
       date={event.date}
       start_time={event.start_time}
       end_time={event.end_time}
       attendees={event.attendees}
+      hostEventShow={props.hostEventShow} 
     />
   ));
 
