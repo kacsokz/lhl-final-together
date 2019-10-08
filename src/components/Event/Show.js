cd2adad73fd91465ced7b6a8dd1b4fbd1dc78671
Ahmed <sultan_902002@yahoo.com>
2019-10-08 00:14:06 -0600

    add codition to display join button

--- src/components/Event/Show.js
+++ src/components/Event/Show.js
@@ -1,39 +1,55 @@
 import React from "react";
 
 import UserAvatar from '../Common/Avatar';
+import queryString from 'query-string'
 
 export default function Show(props) {
+
+  const user_id = queryString.parse(window.location.search).user_id
   return (
     <main className="event__card event__card--show">
 
       <h1 className="event__card--header text--header">HOST</h1>
 
       <section className="event__card--show-body-host">
-        <UserAvatar src={props.avatar}/>
-        <h1 className="event__card--show-body-host-name text--body">{props.user_name}</h1>
-        <h3 className="event__card--show-body-host-tag text--body">{props.tag_line}</h3>
+        <UserAvatar src={props.event.avatar} />
+        <h1 className="event__card--show-body-host-name text--body">{props.event.host_name}</h1>
+        <h3 className="event__card--show-body-host-tag text--body">{props.event.tag_line}</h3>
       </section>
 
       <h1 className="event__card--header text--header">EVENT</h1>
 
       <section className="event__card--show-body-event">
         <h2 className="event__card--show-body-event-header text--header">WHAT ?</h2>
-        <h3 className="event__card--show-body-event-body text--body">{props.event_name}</h3>
+        <h3 className="event__card--show-body-event-body text--body">{props.event.event_name}</h3>
         <h2 className="event__card--show-body-event-header text--header">WHEN ?</h2>
-        <h3 className="event__card--show-body-event-body text--body">{props.date} from {props.start_time} to {props.end_time}</h3>
+        <h3 className="event__card--show-body-event-body text--body">{props.event.event_date} from {props.event.event_start_time} to {props.event.event_end_time}</h3>
         <h2 className="event__card--show-body-event-header text--header">WHERE ?</h2>
-        <h3 className="event__card--show-body-event-body text--body">{props.bar_name}</h3>
+        <h3 className="event__card--show-body-event-body text--body">{props.event.bar_name}</h3>
       </section>
-
-      <h1 className="event__card--header text--header">JOIN {props.attendees} OTHERS</h1>
+      {!user_id && (
+        <h1 className="event__card--header text--header">JOIN {props.event.attendees_count} OTHERS</h1>
+      )}
       <section className="event__card--show-footer">
-        <img
-        style={{ cursor: 'pointer' }}
+        {!user_id && (<img
+          style={{ cursor: 'pointer' }}
           className="event__card--show-footer-join"
           src="images/add-64.png"
           alt="Join"
-          onClick={() => props.onJoin(props.event_id)}
+          onClick={() => props.onJoin(user_id, props.event.event_id, props.event.bar_id)}
         />
+        )}
+        {user_id && (
+          <h1 className="event__card--header text--header">GOING WITH {props.event.attendees_count} OTHERS</h1>
+        )}
+        {user_id && user_id !== props.event.host_id && (<img
+          style={{ cursor: 'pointer' }}
+          className="event__card--show-footer-join"
+          src="images/minus-64.png"
+          alt="Join"
+          onClick={() => props.onJoin(user_id, props.event.event_id, props.event.bar_id)}
+        />
+        )}
       </section>
 
     </main>
