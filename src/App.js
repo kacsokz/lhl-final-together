87eb00bd40b3234567d88431ea279048f0949de0
kacsokz <caseysokach@gmail.com>
2019-10-08 10:02:22 -0600

    Style application and buttons

--- src/App.js
+++ src/App.js
@@ -37,42 +37,51 @@ export default function App() {
   }
 
   return (
-    <div className="App">
-      <main className="layout">
+    <main className="frame">
+
+      {/* Nav Section */}
+      <section className="frame__navbar">
         <NavBarFinal />
-        
-        <div className="main-container">
-          {mode === MAINVIEW && (
-            <div className="right-side">
-                {state.allEvents.length > 0
-                ? (<Event localEvents={state.allEvents}></Event>)
-                : 'Loading...'}
-            </div>
-          )}
+      </section>
 
-          {mode === PROFILEVIEW && state.userInfo && (
-            <div className="right-side">
-              <User
-                key={state.userInfo.id}
-                id={state.userInfo.id}
-                name={`${state.userInfo.first_name} ${state.userInfo.last_name}`}
-                email={state.userInfo.email}
-                tag_line={state.userInfo.tag_line}
-                avatar={state.userInfo.avatar}
-                updateProfile={'from application passed from useApplicationData'}
-                hostingMessage="HOSTING"
-                getEventById='function to display single event'
-                attendingMessage="Attending"
-                event={state.event}
-              />
-            </div>
-          )}
-
-          <div className="left-side">
-            <Map bars={state.allBars}/>
+      {/* Map Section */}
+      <section className="frame__map">
+      {/* <div className="left-side"> */}
+        <Map bars={state.allBars}/>
+      </section>
+      
+      {/* Card Section */}
+      <section className="frame__card">
+      {/* <div className="main-container"> */}
+        {mode === MAINVIEW && (
+          <div>
+          {/* <div className="right-side"> */}
+              {state.allEvents.length > 0
+              ? (<Event localEvents={state.allEvents}></Event>)
+              : 'Loading...'}
           </div>
-        </div>
-      </main>
-    </div>
+        )}
+
+        {mode === PROFILEVIEW && state.userInfo && (
+          <div>
+          {/* <div className="right-side"> */}
+            <User
+              key={state.userInfo.id}
+              id={state.userInfo.id}
+              name={`${state.userInfo.first_name} ${state.userInfo.last_name}`}
+              email={state.userInfo.email}
+              tag_line={state.userInfo.tag_line}
+              avatar={state.userInfo.avatar}
+              updateProfile={'from application passed from useApplicationData'}
+              hostingMessage="HOSTING"
+              getEventById='function to display single event'
+              attendingMessage="Attending"
+              event={state.event}
+            />
+          </div>
+        )}
+      </section>
+      
+    </main>
   );
 }
--- src/components/Common/Button.js
+++ src/components/Common/Button.js
@@ -5,7 +5,8 @@ import classnames from 'classnames/bind';
 export default function Button(props) {
    const buttonClass = classnames("button", {
       "button--red text--body": props.red,
-      "button--blue text--body": props.blue
+      "button--blue text--body": props.blue,
+      "button--nav text--body": props.nav
    });
 
    return (
